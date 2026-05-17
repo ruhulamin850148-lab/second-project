33919c4c620a379ea6638d1fced86c3da2b96a49
Ruhul Amin <ruhul@amin.com>
2026-05-17 19:26:18 +0600

    some modification app.js

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 var myDate = new Date();
+console.log(myDate.getDay());
+console.log(myDate.toDateString());
+console.log(myDate.toString());
 console.log(myDate.toTimeString());
+console.log(myDate.getDate());
+console.log(myDate.getDay());
+console.log(myDate.getFullYear());
+console.log(myDate.getHours());
+console.log(myDate.getMilliseconds());
+console.log(myDate.getMinutes());
+console.log(myDate.getMonth());
+console.log(myDate.getSeconds());
+console.log(myDate.getTime());
+
