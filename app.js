472145f8a878c6c01726ba401ef1e63d440fb30a
Ruhul Amin <ruhul@amin.com>
2026-05-17 22:10:27 +0600

    some modification app.js code

--- app.js
+++ app.js
@@ -1,15 +1,58 @@
-var myDate = new Date();
-console.log(myDate.getDay());
-console.log(myDate.toDateString());
-console.log(myDate.toString());
-console.log(myDate.toTimeString());
-console.log(myDate.getDate());
-console.log(myDate.getDay());
-console.log(myDate.getFullYear());
-console.log(myDate.getHours());
-console.log(myDate.getMilliseconds());
-console.log(myDate.getMinutes());
-console.log(myDate.getMonth());
-console.log(myDate.getSeconds());
-console.log(myDate.getTime());
+//basic javascript code//
+console.log("hello world");
 
+// output power jonno console.log baborhar//
+
+//variable ar babohar see//
+// camelCase: friendName//
+// string variable//
+var friendName = "Prince Ruhul";
+console.log(friendName);
+// stringtype variable decelear //
+// typeof ar work ata kon type ar variable ta decelear korar jonno//
+var strType = "Ruhul Programmer";
+console.log(typeof strType);
+
+// number variable//
+var friendName = 20;
+console.log(friendName);
+ 
+var friendName = 20;
+console.log( typeof friendName);
+
+var numType =  50;
+console.log( typeof numType);
+
+var numberType = `Ruhul,Rajon,Ebrahim`;
+console.log(typeof  numType);
+
+var numbersType = 201050520460210;
+console.log( typeof numbersType);
+
+
+// boolean type variable //
+var boolType = true;
+console.log(boolType);
+
+var boolType = true;
+console.log( typeof boolType);
+
+var boolTypes = false;
+console.log( boolTypes);
+
+var boolType = false;
+console.log( typeof boolType);
+
+// toUpper case and lowerCase mathord //
+var rochona = "My school is one of the most important places in my life. The name of my school is Green Valley High School. It is situated near my house. It is a beautiful and well-known school in our area.";
+console.log(rochona.toUpperCase());
+
+var rochonaName = "MY SCHOOL IS ONE OF THE MOST IMPORTANT PLACES IN MY LIFE. THE NAME OF MY SCHOOL IS GREEN VALLEY HIGH SCHOOL. IT IS SITUATED NEAR MY HOUSE. IT IS A BEAUTIFUL AND WELL-KNOWN SCHOOL IN OUR AREA.";
+console.log(rochonaName.toLowerCase());
+
+// split method ar babohar // split র্অথ বাঘ করা//
+var myFd = "Ruhul Rajon Ebrahim Rifat Faysal";
+console.log(myFd.split("Ruhul"));
+
+var myFriends = "Ruhul Rajon Rahol Basha Khan";
+console.log(myFriends.split("Rahol"));
